7f6223bb812c8bdd7843c892e85053e0fab2be43
Fake Name <fakename@fake.com>
2023-09-21 06:49:45 +1000

    Change base to vtest-7

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vtest-6",
+  base: "/vtest-7",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
