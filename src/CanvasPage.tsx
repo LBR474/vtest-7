e6ab5865d3ae5eef152145c2507a1ce4e8049d8a
Fake Name <fakename@fake.com>
2023-09-21 07:28:00 +1000

    Change final message

--- src/CanvasPage.tsx
+++ src/CanvasPage.tsx
@@ -291,7 +291,7 @@ const CanvasPage: React.FC<Props> = ({ title, loggedIn }) => {
         </Canvas>
       </div>
       <div className="final-content">
-        Let me integrate a Blender 3D model into your React project.
+        MC Software Solutions - for all your secure, distributed-application programming needs
       </div>
     </div>
   );
