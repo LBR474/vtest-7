bb72f80bf0ba51c327deb7e74878c0795c8b63a2
Fake Name <fakename@fake.com>
2024-04-05 10:21:39 +1100

    Add hash tag links

--- src/CanvasPage.tsx
+++ src/CanvasPage.tsx
@@ -54,6 +54,112 @@ const CanvasPage: React.FC<Props> = ({ title, loggedIn }) => {
     emissiveIntensity: 2,
   });
 
+  
+  const showIntroContent = (index: number) => {
+    if (index == 0) {
+      gsap.to("#intro", {
+        zIndex: 1, // Bring the #intro div to the front
+        opacity: 1,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+      gsap.to(".penultimate-content", {
+        opacity: 0,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+    } else if (index == 1) {
+      gsap.to("#work", {
+        zIndex: 1, // Bring the #work div to the front
+        opacity: 1,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+      gsap.to(".penultimate-content", {
+        opacity: 0,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+    } else if (index == 2) {
+      gsap.to("#simulations", {
+        zIndex: 1, // Bring the #work div to the front
+        opacity: 1,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+      gsap.to(".penultimate-content", {
+        opacity: 0,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+    } else if (index == 3) {
+      gsap.to("#about", {
+        zIndex: 1, // Bring the #work div to the front
+        opacity: 1,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+      gsap.to(".penultimate-content", {
+        opacity: 0,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+    } else if (index == 4) {
+      gsap.to("#contact", {
+        zIndex: 1, // Bring the #work div to the front
+        opacity: 1,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+      gsap.to(".penultimate-content", {
+        opacity: 0,
+        duration: 0.1,
+        ease: "power2.inOut",
+      });
+    }
+  };
+
+  const hideIntroContent = () => {
+    gsap.to("#intro, #work, #simulations, #about, #contact", {
+      zIndex: -5, // Bring the #intro div to the front
+      opacity: 0,
+      duration: 0.1,
+      ease: "power2.inOut",
+    });
+     gsap.to(".penultimate-content", {
+       opacity: 1,
+       duration: 0.1,
+       ease: "power2.inOut",
+     });
+  };
+  const handleClick = (index: number) => {
+    switch (index) {
+      case 0:
+        showIntroContent(index);
+        break;
+      case 1:
+        showIntroContent(index);
+        break;
+      case 2:
+        showIntroContent(index);
+        break;
+      case 3:
+        showIntroContent(index);
+        break;
+      case 4:
+        showIntroContent(index);
+        break;
+      // Add more cases for other sections as needed
+      default:
+        break;
+    }
+  };
+
+  const handleClickHide = () => {
+    hideIntroContent();
+  };
+
+
   useEffect(() => {
     const timer = (ms: number | undefined) =>
       new Promise((res) => setTimeout(res, ms));
@@ -303,24 +409,205 @@ const CanvasPage: React.FC<Props> = ({ title, loggedIn }) => {
           <nav>
             <ul>
               <li>
-                <a href="#intro">Intro</a>
+                <a onClick={() => handleClick(0)} href="#intro">
+                  Intro
+                </a>
               </li>
               <li>
-                <a href="#work">Work</a>
+                <a onClick={() => handleClick(1)} href="#work">
+                  Work
+                </a>
               </li>
               <li>
-                <a href="#simulations">Simulations</a>
+                <a onClick={() => handleClick(2)} href="#simulations">
+                  Simulations
+                </a>
               </li>
               <li>
-                <a href="#about">About</a>
+                <a onClick={() => handleClick(3)} href="#about">
+                  About
+                </a>
               </li>
               <li>
-                <a href="#contact">Contact</a>
+                <a onClick={() => handleClick(4)} href="#contact">
+                  Contact
+                </a>
               </li>
             </ul>
           </nav>
         </div>
       </div>
+      <article id="intro">
+        <div className="close_X">
+          <a href="#" onClick={handleClickHide}>
+            {" "}
+            <h4> X </h4>
+          </a>
+        </div>
+        <header>Intro</header>
+        <p>
+          Bespoke software solutions provider/software architecture consultancy
+          for the corporate sector. Builder of quality, redundant, scalable,
+          flexible systems (specialising in microservices architectures).
+        </p>
+        <p>
+          Specialties: Sports betting and financial trading solutions,
+          distributed applications development, inter-process messaging,
+          third-party API integration, high-level architectural design,
+          large-scale statistical analysis, .NET development. Check out a sample
+          of our <a href="#work">work</a>.
+        </p>
+        <p>
+          Roles: Applications/solutions/systems architect, technical team lead,
+          data modeller, data migration specialist, principal software engineer.
+        </p>
+      </article>
+      <article id="work">
+        <div className="close_X">
+          <a href="#" onClick={handleClickHide}>
+            {" "}
+            <h4> X </h4>
+          </a>
+        </div>
+        <header>Work</header>
+        <p>
+          Responsible for the architecture, design, development, deployment, and
+          support of a distributed automated low-latency exchange horse racing
+          trading system implementing a real-time trading algorithm based on
+          runner probabilities. This system covered the three main sports
+          betting exchanges: Betfair, Betdaq, and Matchbook. This was a solo
+          effort, from start to finish, and I built the system from concept
+          through to deployment and support, based on the same framework as
+          previous sports betting systems. At peak, turnover through the system
+          was around $200M per year.
+        </p>
+        <p>
+          Responsible for the architecture, design, development, deployment, and
+          support of a distributed automated sports betting system. Working on
+          the system on a full-time basis, the system was essentially
+          &quot;built from scratch&quot; for one of the biggest syndicates in
+          the Australian sports betting market, employing over 10 full-time
+          staff, with an annual turnover more than $100M. My role included full
+          control over the software architecture and data model, leading the
+          development team, managing ongoing deployment of the live system and
+          associated updates, and supporting the system 24 hours per day, 365
+          days per year. The system comprised over 50 inter-connected
+          message-based applications, a 500 GB database with billions of
+          records, and pushed out over a million message-based data updates
+          daily to multiple client applications.
+        </p>
+        <p>
+          Re-architected/re-designed an alarm monitoring system that (still)
+          monitors all branches and ATMs for one of Australia's major financial
+          institutions. The initial system was deployed in 2006, with a major
+          upgrade in 2007, and again in 2010 to migrate from a Windows-based GUI
+          to a web-based GUI. The system comprises multiple logical and physical
+          tiers, connected through a message-based framework implemented over
+          TCP. System reliability is such that no server application has needed
+          a restart since the last update 3 years ago, apart from that required
+          due to infrastructure maintenance.
+        </p>
+      </article>
+      <article id="simulations">
+        <div className="close_X">
+          <a href="#" onClick={handleClickHide}>
+            {" "}
+            <h4> X </h4>
+          </a>
+        </div>
+        <header>Simulations</header>
+        <p>
+          How quickly can you simulate a tennis match by assigning each player a
+          "known" winning serve percentage?
+        </p>
+        <p>
+          Well, if you try really hard, and use lookups and cram everything into
+          the L1 cache, pretty darn fast!
+        </p>
+        <p>
+          I developed code that averages around 12 clock cycles per point
+          (around 3 nanoseconds), and I performed 30 trillion (that's
+          30,000,000,000,000) match simulations, in a week, for differing
+          winning serve percentages and differing "starting scores" for a
+          standard 3-set match decided by a first-to-7 point tiebreaker (on a
+          hosted server I run).
+        </p>
+      </article>
+      <article id="about">
+        <div className="close_X">
+          <a href="#" onClick={handleClickHide}>
+            {" "}
+            <h4> X </h4>
+          </a>
+        </div>
+        <header>About</header>
+        <p>
+          MC Software Solutions is run by Misha Charrett. Misha has more than 30
+          years' experience as a quality software engineer building complex
+          systems with high reliability. Specialising in message-based,
+          distributed, multi-threaded applications (built using a microservices
+          architectural framework), Misha has an exceptional ability to distil
+          complex multi-faceted problems down to simpler component-based
+          solutions.
+        </p>
+      </article>
+      <article id="contact">
+        <div className="close_X">
+          <a href="#" onClick={handleClickHide}>
+            {" "}
+            <h4> X </h4>
+          </a>
+        </div>
+        <header>Contact</header>
+        <form method="get" action="test.aspx">
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message"></textarea>
+          </div>
+          <ul className="actions">
+            <li>
+              <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+              <input type="reset" value="Reset" />
+            </li>
+          </ul>
+        </form>
+        <ul className="icons">
+          <li>
+            <a
+              href="https://twitter.com/misha_mcss"
+              className="icon fa-twitter"
+            >
+              <span className="label">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/misha.mcss"
+              className="icon fa-facebook"
+            >
+              <span className="label">Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/mishacharrett/"
+              className="icon fa-linkedin"
+            >
+              <span className="label">LinkedIn</span>
+            </a>
+          </li>
+        </ul>
+      </article>
     </div>
   );
 };
